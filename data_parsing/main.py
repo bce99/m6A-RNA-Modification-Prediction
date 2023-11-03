603c9053a8d817f0b9b38c4c3ade0d2e0fc7a60e
burdenbc <garfieldbc53@gmail.com>
2023-11-03 11:40:58 +0800

    add instruction to run main.py

--- data_parsing/main.py
+++ data_parsing/main.py
@@ -5,6 +5,10 @@ import json
 from load_and_store_data_info import *
 from parse_data import *
 
+# To execute the loading and parsing script
+# open your terminal and navigate to the current directory
+# run 'python main.py'
+
 if __name__ == "__main__":
     load_and_store_data_info()
     run_parse_data()
