59eb2d762953547f20cf0c319aee11faf91585eb
Bian Ce <e0559243@UT2023-MAC09.local>
2023-11-04 15:10:17 +0800

    update dummy features

--- data_parsing/parse_data.py
+++ data_parsing/parse_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import gzip
 import json
+from collections import Counter
 
 def read_json_gz(file_path:str):
     # Create a list to hold the JSON objects
@@ -18,19 +19,39 @@ def read_json_gz(file_path:str):
     
     return (data_list, file_path)
 
-def parse_data(data_list):
-    parsed_data = []
+def count_acgt_letters(input_string):
+    letter_counts = dict(Counter(input_string))
+    acgt_counts = {
+        'A': letter_counts.get('A', 0),
+        'C': letter_counts.get('C', 0),
+        'G': letter_counts.get('G', 0),
+        'T': letter_counts.get('T', 0),
+    }
+    
+    return acgt_counts
+
+def count_data(data_list):
+    parsed_data = []  
     for data in data_list:
         for transcript_id, positions in data.items():
             for position, details in positions.items():
                 for sequence, readings in details.items():
+                    letter_counts = count_acgt_letters(sequence[1:6]) 
                     for reading in readings:
-                        parsed_data.append([transcript_id, int(position), sequence] + reading)
-    return pd.DataFrame(parsed_data, columns=['transcript_id', 'position', 'sequence', '-1 length', '-1 sd', '-1 mean', '0 length', '0 sd', '0 mean', '+1 length', '+1 sd', '+1 mean'])
+                        value = reading[3:6]
+                        parsed_data.append([transcript_id, int(position), sequence[1:6],letter_counts['A'], letter_counts['C'], letter_counts['G'], letter_counts['T']] + value)
+
+    columns = ['transcript_id', 'position','sequence', 'A', 'C', 'G', 'T', '0 length', '0 sd', '0 mean']
+
+    return pd.DataFrame(parsed_data, columns=columns)
 
 def output_parsed_data(data_list, file_path):
-    df = parse_data(data_list)
-    df.to_csv(f'DSA4266Processed_dataset{file_path[7]}_json.csv', index=False)
+    df = count_data(data_list)
+    df['0 min'] = df['0 mean'] - df['0 sd']*1.96
+    df['0 max'] = df['0 mean'] + df['0 sd']*1.96
+    df = pd.get_dummies(df, columns=['sequence'], prefix='sequence')
+    
+    df.to_csv(f'Parsed_Data{file_path[7]}.csv', index=False)
     
 
 def run_parse_data():
